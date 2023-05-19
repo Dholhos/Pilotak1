6a72e2e5bbac1f5c33a7ef85ff1b2dd6c9879c76
FeketeClaw <FeketeClaw@REVISION-PC>
2023-05-19 10:12:17 +0000

    Szünet előtti mentés

---
+++ ConsoleApp4/Pilotak.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Pilotak
+    {
+        string nev;
+        string szuletesiDatum;
+        string nemzetiseg;
+        int rajtszám;
+
+        public Pilotak(string nev, string szuletesDatum, string nemzetiseg, int rajtszám)
+        {
+            this.nev = nev;
+            this.szuletesiDatum = szuletesDatum;
+            this.nemzetiseg = nemzetiseg;
+            this.rajtszám = rajtszám;
+        }
+        public string Nev { get => nev; }
+        public string SzuletesiDatum { get => szuletesiDatum; }
+        public string Nemetiseg { get => nemzetiseg; }
+        public int Rajtszam { get => Rajtszam; }
+    }
+}
