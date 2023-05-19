457dbd30ea6b24253b4b5328723d03095314445c
FeketeClaw <FeketeClaw@REVISION-PC>
2023-05-19 10:35:50 +0000

    2. commit

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,10 +1,25 @@
-﻿
+﻿using System.IO;
+using System.Data.SqlTypes;
+using ConsoleApp4;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
-        List<Pilotak> lista = new List<Pilotak>;
-        Console.WriteLine();
-
+        List<string> lista = new List<string>();
+        StreamReader sr = new StreamReader("Pilotak.csv");
+        int adatSor = 0;
+        string sor = sr.ReadLine();
+        while (sor != null)
+        {
+            lista.Add(sor);
+            adatSor++;
+        }
+        sr.Close();
+        Console.WriteLine("3. feladat: {0}",adatSor);
+        string seged = lista.Last();
+        Console.WriteLine("4. feladat: {0}", seged[0]);
+        Console.WriteLine("5. feladat");
     }
+    
 }
