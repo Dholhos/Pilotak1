666ce2ee19b31b4838e78cf771287c69144846a5
FeketeClaw <FeketeClaw@REVISION-PC>
2023-05-19 10:52:25 +0000

    Végső commit

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -16,9 +16,18 @@ internal class Program
             adatSor++;
         }
         sr.Close();
-        Console.WriteLine("3. feladat: {0}",adatSor);
+        Console.WriteLine("3. feladat: {0}",adatSor-1);
+
         string seged = lista.Last();
         Console.WriteLine("4. feladat: {0}", seged[0]);
+
+        foreach (var item in lista)
+        {
+            if ((item[1] < 1901)
+            {
+                Console.WriteLine(item[0], item[1]);
+            }
+        }
         Console.WriteLine("5. feladat");
     }
     
